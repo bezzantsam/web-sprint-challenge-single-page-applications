4371490dbadb30f45396c6f927c4681c4b7e2af5
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2021-12-16 22:01:57 -0800

    App links and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./components/homepage";
+import Help from "./components/Help";
+import Shop from "./components/Shop";
+import Confirm from "./components/Confirm";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <nav>
+        <h1 className="bloomtech-pizza">Bloomtech Pizza</h1>
+        <p className="nom">Num Num Num</p>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/help">Help</Link>
+          <Link to="/order-pizza">Pizza</Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 export default App;
---
+++ src/components/Confirm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Confirm = () => {
+  return (
+    <div>
+      <H1> Your Order Is Complete </H1>
+    </div>
+  );
+};
+
+export default Confirm;
